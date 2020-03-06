edd449f059dfd9ba9b4135bf83f2d1eeb1848f1a
yingtlee <lty3105@gmail.com>
2020-03-06 18:55:01 +0800

    New project robofriends

---
+++ src/container/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import CardList from '../component/CardList';
+import SearchBox from '../component/SearchBox';
+import './App.css';
+import Scroll from '../component/Scroll';
+
+class App extends Component{
+    constructor(){
+        super();
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}));
+    }
+
+    onSearchChange = (event) =>{
+        this.setState({ searchfield: event.target.value });
+    }
+
+    render(){
+        const {robots, searchfield} = this.state;
+        const filterRobots = robots.filter(robot=>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+        });
+
+        if(robots.length ===0){
+            return (<h1 className='f1'>Loading</h1>);
+        }
+        else{            
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <CardList robots={filterRobots}/>
+                    </Scroll>
+                </div>
+            );
+        }
+    }
+}
+
+export default App;
